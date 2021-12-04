27bb63f0597e2b28b5e2c4bd8a22e49b93821919
Bryan Nehl <k0emt@dbbear.com>
2021-12-03 22:54:04 -0500

    start templates for the days

---
+++ day-19/main_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestFunction(t *testing.T) {
+
+	// actual := functionUnderTest()
+	// expected := XXXX
+
+	// if actual != expected {
+	// 	t.Errorf("Expected %v, but got %v", expected, actual)
+	// }
+}
